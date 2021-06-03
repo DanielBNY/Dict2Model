f12988a5d8f9860680487d1794a78ccb41eba2c9
Daniel <daniel.benyaacov@gmail.com>
2021-06-04 01:40:19 +0300

    Add a test object and dict

--- main.py
+++ main.py
@@ -16,6 +16,24 @@ If the variable NUMBER_SOURCE would change to NUMBER_
 """
 
 
+class TestObj:
+    number: int
+    question: bool
+
+    NUMBER_SOURCE = "data-number"  # dict source
+    QUESTION_SOURCE = "data-question"  # dict source
+    QUESTION_TYPE_ERROR = Exception  # when the source does not exist
+    NUMBER_DOES_NOT_EXIST = None
+
+
+dict_input_test = {
+    "data": {
+        "number": 123,
+        "question": True
+    }
+}
+
+
 def is_valid_variable_names(annotations_names, global_dict):
     for name in annotations_names:
         if f"{name.upper()}_SOURCE" not in global_dict:
