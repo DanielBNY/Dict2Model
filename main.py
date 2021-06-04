7b55c86c41a1b0c089a88a6aca24c77dd81e8f75
Daniel <daniel.benyaacov@gmail.com>
2021-06-04 11:06:33 +0300

    Add a get input from source and an insert values to class

--- main.py
+++ main.py
@@ -44,7 +44,24 @@ class SpecialDictModel:
 
     def run(self):
         self.validate_usages_in_class()
+        self.insert_values_to_class()
         return self.input_class
+
+    def insert_values_to_class(self):
+        for variable_name in self.class_annotations:
+            variable_input = self.get_input_from_source(variable_name)
+            if type(variable_input) is self.class_annotations[variable_name]:
+                setattr(self.input_class, variable_name, variable_input)
+            else:
+                setattr(self.input_class, variable_name, None)
+
+    def get_input_from_source(self, variable_name):
+        source = self.class_attr.get(f"{variable_name.upper()}_SOURCE")
+        split_source = source.split('-')
+        data = self.input_data
+        for part in split_source:
+            data = data.get(part)
+        return data
 
     # Usages Validations
 
@@ -56,3 +73,8 @@ class SpecialDictModel:
             if f"{name.upper()}_SOURCE" not in self.class_attr:
                 raise Exception(SOURCE_NAMING_EXCEPTION_MESSAGE)
         return True
+
+
+def test():
+    test_obj: TestObj = SpecialDictModel(TestObj, dict_input_test).run()
+    print(test_obj.number, test_obj.question)
