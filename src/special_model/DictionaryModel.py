92138298310c6a1d39f2a00ac588d41e70128974
Daniel <daniel.benyaacov@gmail.com>
2021-06-05 19:31:37 +0300

    Change to relative path

--- src/special_model/DictionaryModel.py
+++ src/special_model/DictionaryModel.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from DictionaryModelConsts import *
+from .DictionaryModelConsts import *
 
 
 class DictionaryModelFactory:
