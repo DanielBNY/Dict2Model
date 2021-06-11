c6eeb6e8a04444028211dec10b68f6bffe7a3c20
Daniel <daniel.benyaacov@gmail.com>
2021-06-11 23:54:04 +0300

    Remove type annotations

--- src/dict2model/models.py
+++ src/dict2model/models.py
@@ -2,7 +2,7 @@ SOURCE_INFO_KEY = 'source_info_obj_dict'
 
 
 class SourceInfo:
-    def __init__(self, path: list, type_exception: bool, path_exception: bool, required_type: type):
+    def __init__(self, path, type_exception, path_exception, required_type):
         self.path = path
         self.type_exception = type_exception
         self.path_exception = path_exception
@@ -21,14 +21,14 @@ class MetaModel(type):
         except AttributeError:
             return None
 
-    def get_source_info_obj_dict(cls) -> dict:
+    def get_source_info_obj_dict(cls):
         source_info_obj_dict = cls.__get__(SOURCE_INFO_KEY)
         if source_info_obj_dict is None:
             return {}
         return source_info_obj_dict
 
-    def source(cls, path: list, required_type: type, type_exception=True,
-               path_exception=True) -> object:
+    def source(cls, path, required_type, type_exception=True,
+               path_exception=True):
         cls.validate_source_input(path, required_type, type_exception, path_exception)
         new_source_info = SourceInfo(path=path, path_exception=path_exception,
                                      type_exception=type_exception, required_type=required_type)
@@ -117,6 +117,6 @@ class Example(Dict2Model):
     b = Dict2Model.source(path=['j'], required_type=int, type_exception=False, path_exception=False)
 
 
-example2: Example = ModelFactory(Example).use({'a': 3, 'j': 3})
+example2 = ModelFactory(Example).use({'a': 3, 'j': 3})
 print(example2.a, example2.b)
 print(Example.__dict__)
